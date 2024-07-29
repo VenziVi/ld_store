7e7ca3c7d62d042d5acfd3fcf2fae3d2e2b2502d
Venci Lazarov <vlazarov86@gmail.com>
2024-07-29 23:56:27 +0300

    add some views

--- ld_store/store/urls.py
+++ ld_store/store/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('', views.sore, name='store'),
-
+path('', views.store, name='store'),
+path('product/<slug:slug>/', views.product_info, name='product-info'),
 ]
--- ld_store/store/views.py
+++ ld_store/store/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from . models import Category, Product
+from django.shortcuts import get_object_or_404
 
-def sore(request):
-    return render(request, 'store/store.html')
+def store(request):
+    products = Product.objects.all()
+    context = {'all_products': products}
+
+    return render(request, 'store/store.html', context)
+
+def categories(request):
+    categories = Category.objects.all()
+    return {'all_categories': categories}
+
+def product_info(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    context = {'product': product}
+    return render(request, 'store/product-info.html', context)
