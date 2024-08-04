ed2dcf4d31614eda8151b25e73a1f5fb0b1a8709
Venci Lazarov <vlazarov86@gmail.com>
2024-08-04 14:16:51 +0300

    admin page updates

--- ld_store/store/models.py
+++ ld_store/store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Category(models.Model):
     name = models.CharField(max_length=250, db_index=True)
@@ -25,7 +26,7 @@ class Product(models.Model):
     title = models.CharField(max_length=250)
     brand = models.CharField(max_length=250, default='un_branded')
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=4, decimal_places=2)
+    price = models.DecimalField(max_digits=5, decimal_places=2, validators=[MinValueValidator(0.1), MaxValueValidator(500)])
     quantity = models.PositiveIntegerField(default=0)
     slug = models.SlugField(max_length=250)
     image = models.ImageField(upload_to='images/')
